1a454f491251a8ec56daff722dc357efc5c9d098
Tonya Fletcher <tfletch4504@gmail.com>
2021-03-25 16:58:54 -0400

    messages

--- index.js
+++ index.js
@@ -1,7 +1,67 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+//Create an array of questions for user input
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'Please enter a title',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please enter instructions for installation',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please enter usage information',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Please select a license',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Please select a badge',
+        name: 'badge',
+    },
+    {
+        type: 'input',
+        message: 'Please enter contribution guidelines',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Please enter test instructions',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github username',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Please enter the link to this Github repo',
+        name: 'repo',
+    },
+])
+    .then((response) => {
+        console.log(response);
+
+    })
+// const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
