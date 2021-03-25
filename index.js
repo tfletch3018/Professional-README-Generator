782e606ba31b2d10b6d7a6f61c2ff901e2a11071
Tonya Fletcher <tfletch4504@gmail.com>
2021-03-25 19:59:14 -0400

    licenses

--- index.js
+++ index.js
@@ -1,73 +1,78 @@
 //Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-
+const generate = require("./utils/generateMarkdown");
+const { url } = require("inspector");
 
 //Create an array of questions for user input
 inquirer
-.prompt([
-    {
-        type: 'input',
-        message: 'Please enter a title',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'Please enter a description',
-        name: 'description',
-    },
-    {
-        type: 'input',
-        message: 'Please enter instructions for installation',
-        name: 'installation',
-    },
-    {
-        type: 'input',
-        message: 'Please enter usage information',
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'Please select a license',
-        name: 'license',
-    },
-    {
-        type: 'input',
-        message: 'Please select a badge',
-        name: 'badge',
-    },
-    {
-        type: 'input',
-        message: 'Please enter contribution guidelines',
-        name: 'contribution',
-    },
-    {
-        type: 'input',
-        message: 'Please enter test instructions',
-        name: 'tests',
-    },
-    {
-        type: 'input',
-        message: 'Please enter your Github username',
-        name: 'username',
-    },
-    {
-        type: 'input',
-        message: 'Please enter the link to this Github repo',
-        name: 'repo',
-    },
-])
+    .prompt([
+        {
+            type: 'input',
+            message: 'Please enter a title',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'Please enter a description',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please enter instructions for installation',
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: 'Please enter usage information',
+            name: 'usage',
+        },
+        {
+            type: 'list',
+            message: 'Please select a license',
+            name: 'license',
+            choices: [url("")]
+        },
+        {
+            type: 'input',
+            message: 'Please select a badge',
+            name: 'badge',
+        },
+        {
+            type: 'input',
+            message: 'Please enter contribution guidelines',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'Please enter test instructions',
+            name: 'tests',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your Github username',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'Please enter the link to this Github repo',
+            name: 'repo',
+        },
+    ])
     .then((response) => {
         console.log(response);
 
-    })
-// const questions = [];
+        const questions = `${response.title.toLowerCase().split(" ").join("")}.json`
 
+        fs.appendFile(questions, JSON.stringify(response, null, '\t'), (err) =>
+            err ? console.error(err) : console.log("created files successfully")
+        )
+    });
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
